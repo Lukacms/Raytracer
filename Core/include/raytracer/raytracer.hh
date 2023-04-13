c7a23f30e066ad5c26e711336efcb4b9e7c8862d
Luka Camus <luka.camus@epitech.eu>
2023-04-13 09:29:48 +0200

    [ADD] basic project starter

---
+++ Core/include/raytracer/raytracer.hh
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2023
+** raytracer
+** File description:
+** raytracer
+*/
+
+#pragma once
+
+constexpr int SUCCESS = 0;
+constexpr int FAILURE = 84;
---
+++ Core/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** src
+** File description:
+** main
+*/
+
+#include <raytracer/raytracer.hh>
+
+int main(int argc, const char *argv[])
+{
+    return SUCCESS;
+}
