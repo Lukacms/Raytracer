f9852402056a084654f1c2f75f934169bb44b232
edouard.pradie <edouard.pradie@epitech.eu>
2023-04-25 11:34:43 +0200

    [FIX] light positon and coef cromatique

--- Lights/AmbiantLight/src/AmbiantLight.cpp
+++ Lights/AmbiantLight/src/AmbiantLight.cpp
@@ -24,8 +24,11 @@ Color light::AmbientLight::lighten(HitInfos &infos, Color color)
     math::Vector3D light{this->m_position.getX() - infos.point.getX(),
                          this->m_position.getY() - infos.point.getY(),
                          this->m_position.getZ() - infos.point.getZ()};
+    light /= light.length();
     double rho = light.dot(infos.normal);
 
+    if (rho > 1)
+        return color;
     if (rho <= 0)
         return Color{0, 0, 0};
     // if (rho > 0 && rho <= 1) {
