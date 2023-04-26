2f46dec3d509550626c8f8dea499ed111a217cde
SamuelFlorentin <samuel.florentin@epitech.eu>
2023-04-26 11:50:58 +0200

    [ADD] working core and working ppm transcriptor

--- Core/include/raytracer/factory/LightFactory.hh
+++ Core/include/raytracer/factory/LightFactory.hh
@@ -7,11 +7,11 @@
 
 #pragma once
 
+#include <memory>
 #include <raytracer/ALight.hh>
+#include <raytracer/factory/AFactory.hh>
 #include <raytracer/math/IPrimitive.hh>
 #include <raytracer/math/Point3D.hh>
-#include <raytracer/factory/AFactory.hh>
-#include <memory>
 
 const std::string_view AMBIANT_LIB = "./plugins/raytracer_ambiant_light.so";
 const std::string_view DIRECTIONNAL_LIB = "./plugins/raytracer_directionnal_light.so";
@@ -19,18 +19,21 @@ const std::string_view LOAD_LIGHT_METHOD = "entry_point_light";
 const std::string_view ERROR_LIGHT_CANNOT_LOAD = "RayTracer: Unable to load library.";
 const std::string_view ERROR_NOT_LIGHT = "RayTracer: Not a light library.";
 
-namespace raytracer {
-    class LightFactory : public AFactory {
+namespace raytracer
+{
+    class LightFactory : public AFactory
+    {
         public:
             LightFactory() = default;
-            LightFactory(const LightFactory&) = default;
+            LightFactory(const LightFactory &) = default;
             LightFactory(LightFactory &&) = default;
             ~LightFactory() = default;
-    
-            LightFactory &operator=(const LightFactory&) = default;
+
+            LightFactory &operator=(const LightFactory &) = default;
             LightFactory &operator=(LightFactory &&);
 
             static std::unique_ptr<light::ILight> createAmbiant(math::Point3D &origin);
+
         private:
     };
-}
+} // namespace raytracer
--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -11,6 +11,7 @@
 #include <memory>
 #include <raytracer/Camera.hh>
 #include <raytracer/Canva.hh>
+#include <raytracer/Core.hh>
 #include <raytracer/Ray.hh>
 #include <raytracer/RayTracer.hh>
 #include <raytracer/factory/PrimitiveFactory.hh>
@@ -22,6 +23,7 @@
 //     return SUCCESS;
 // }
 
+/*
 int main()
 {
     math::Point3D point1{0, 0, 1};
@@ -51,4 +53,36 @@ int main()
         }
     }
     return 0;
+}
+*/
+
+int main()
+{
+    math::Point3D point1{0, 0, 1};
+    math::Point3D point5{-2, 0, -3};
+    math::Point3D point2{-0.5, 0, -2};
+    math::Point3D point6{0, 0, -1};
+    math::Point3D point3{-0.5, -0.5, 0};
+    math::Point3D point4{2, 0, 1};
+    raytracer::Canva canva{point3};
+    // Set up camera
+    raytracer::Camera camera(point1, canva);
+    // Set up Core
+    raytracer::Core core{camera};
+    // Set up sphere
+    std::unique_ptr<math::IPrimitive> sphere =
+        raytracer::PrimitiveFactory::createSphere(point2, 0.5);
+    sphere->setColor(0, 0, 255);
+    std::unique_ptr<math::IPrimitive> sphere2 =
+        raytracer::PrimitiveFactory::createSphere(point5, 0.5);
+    sphere2->setColor(255, 0, 0);
+    std::unique_ptr<math::IPrimitive> sphere3 =
+        raytracer::PrimitiveFactory::createSphere(point6, 0.5);
+    sphere3->setColor(0, 255, 0);
+    std::unique_ptr<light::ILight> light = raytracer::LightFactory::createAmbiant(point4);
+    core.add_object(std::move(sphere));
+    core.add_object(std::move(sphere2));
+    core.add_object(std::move(sphere3));
+    core.add_lights(std::move(light));
+    core.launch();
 }
