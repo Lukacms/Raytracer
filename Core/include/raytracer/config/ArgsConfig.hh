ecb60d375fb332caa55a41abd427643eaba961ed
Luka Camus <luka.camus@epitech.eu>
2023-05-05 17:29:19 +0200

    [ADD] ArgsConfig => class to get arguments
    have a choice: either ppm or sfml display

---
+++ Core/include/raytracer/config/ArgsConfig.hh
@@ -0,0 +1,65 @@
+/*
+** EPITECH PROJECT, 2023
+** config
+** File description:
+** ArgsConfig
+*/
+
+#pragma once
+
+#include <exception>
+#include <string>
+
+constexpr int PPM_NB_ARGS{4};
+constexpr int SFML_NB_ARGS{3};
+
+constexpr std::string_view ARGS_NB_ERROR{"Must be either 3 or 4 arguments, try -h to see options."};
+constexpr std::string_view HELP_INDIC{"-h"};
+
+namespace raytracer
+{
+    enum class DisplayType { PpmOutput, SfmlOutput };
+
+    struct Infos {
+            DisplayType type{DisplayType::SfmlOutput};
+            std::string output{};
+            std::string input{};
+    };
+
+    class ArgsConfig
+    {
+        public:
+            /* ctor / dtor */
+            ArgsConfig() = default;
+            ArgsConfig(ArgsConfig const &to_copy) = default;
+            ArgsConfig(ArgsConfig &&to_move) = default;
+            ~ArgsConfig() = default;
+
+            /* override operators */
+            ArgsConfig &operator=(ArgsConfig const &to_copy) = default;
+            ArgsConfig &operator=(ArgsConfig &&to_move) = default;
+
+            /* exception */
+            class ArgsException : public std::exception
+            {
+                public:
+                    ArgsException(std::string perror);
+                    ArgsException(ArgsException const &to_copy) = default;
+                    ArgsException(ArgsException &&to_move) = default;
+                    ~ArgsException() override = default;
+                    ArgsException &operator=(ArgsException const &to_copy) = default;
+                    ArgsException &operator=(ArgsException &&to_move) = default;
+
+                    /* override method what of exception */
+                    [[nodiscard]] const char *what() const noexcept override;
+
+                private:
+                    std::string msg;
+            };
+
+            /* methods */
+            static Infos getArgs(int argc, const char *const *argv);
+
+        private:
+    };
+} // namespace raytracer
---
+++ Core/src/config/ArgsConfig.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2023
+** config
+** File description:
+** ArgsConfig
+*/
+
+#include <raytracer/RayTracer.hh>
+#include <raytracer/config/ArgsConfig.hh>
+
+/* methods */
+raytracer::Infos raytracer::ArgsConfig::getArgs(int argc, const char *const *argv)
+{
+    Infos infos{};
+
+    if (argc == 2 && argv[1] == HELP_INDIC.data()) {
+        throw ArgsException(HELP_MSG.data());
+    }
+    if (argc == PPM_NB_ARGS) {
+        infos.output = std::string{argv[PPM_NB_ARGS - 1]};
+        infos.type = DisplayType::PpmOutput;
+    } else if (argc == SFML_NB_ARGS) {
+        infos.type = DisplayType::SfmlOutput;
+    } else {
+        throw ArgsException(ARGS_NB_ERROR.data());
+    }
+    infos.input = std::string{argv[1]};
+    return infos;
+}
+
+/* ArgsException */
+raytracer::ArgsConfig::ArgsException::ArgsException(std::string perror) : msg{std::move(perror)} {}
+
+const char *raytracer::ArgsConfig::ArgsException::what() const noexcept
+{
+    return msg.data();
+}
